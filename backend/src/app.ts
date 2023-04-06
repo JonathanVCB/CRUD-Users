799d389ab92b9b900755ef6dc29c1ec3f4257836
JonathanVCB <viniciusjonathan@hotmail.com>
2023-04-06 12:26:25 -0300

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,15 +3,17 @@ import "reflect-metadata";
 import "express-async-errors";
 import usersRoutes from "./routes/usersRoutes";
 import sessionRoutes from "./routes/sessionUserRoutes";
-import contactRoutes from './routes/contactRoutes'
+import contactRoutes from "./routes/contactRoutes";
 import handleError from "./errors/handleError";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", usersRoutes);
 app.use("/login", sessionRoutes);
-app.use("/contact", contactRoutes)
+app.use("/contact", contactRoutes);
 
 app.use(handleError);
 
